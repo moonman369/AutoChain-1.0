b54287247dd64401a26b723852d806e17b42c9bd
"moonman369" <"mightyayan369@gmail.com">
2022-04-14 22:57:14 +0530

    commit-14.4.22

--- contracts/itemmanager.sol
+++ contracts/itemmanager.sol
@@ -1,34 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Item {
-    uint public itemPriceInWei;
-    uint public pricePaid;
-    uint public itemIndex;
-    address public addr;
-
-    constructor(uint _itemPriceInWei, uint _itemIndex, address _addr) public {
-        itemPriceInWei = _itemPriceInWei;
-        itemIndex = _itemIndex;
-        addr = _addr;
-    }
-
-    fallback () external payable {
-        require (pricePaid == 0, "Item is paid for already.");
-        require (itemPriceInWei == msg.value, "Please pay exact amount.");
-        pricePaid += msg.value;
-        //address payable mainAddr = address(parentContract);
-        //parentContract.triggerPayment{value : msg.value}(itemIndex);
-        (bool success, ) =  addr.call{value : msg.value, gas : 80000000 }(abi.encodeWithSignature("triggerPayment(uint256)", itemIndex));
-        require (success, "Transaction unsuccessful, cancelling payment");
-    }
-
-    //fallback () external payable {}
-}
+import "./item.sol";
 
 contract ItemManager {
 
-    enum AutoChainState {Created, Paid, Delivered}
+    enum AutoChainState {Null, Created, Paid, Delivered}
+
+    
 
     struct item {
         Item _item;
@@ -41,6 +20,10 @@ contract ItemManager {
 
     mapping (uint => item) public items;
     uint itemIndex;
+
+    // function getEnum (uint _itemIndex) public view returns (ItemManager.AutoChainState) {
+    //     return items[_itemIndex]._state;
+    // }
 
     function createItem (string memory _identifier, uint _itemPrice) public {
 
